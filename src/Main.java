e3aae541fdd4617ff2f2b204bb7bf236140333f6
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2023-06-17 13:32:21 +0300

    testing the init git

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hi form the test function for the Problems");
+        System.out.println("hi ");
     }
 
 }
